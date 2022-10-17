13afcbd57b066c0fb2c827ab5972d3f083ffe0f5
Daniel Heene <daniel@heene.io>
2022-10-17 04:02:35 +0200

    update

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,22 +1,8 @@
-import * as process from 'node:process';
 import { createCliConfig } from 'sanity/cli';
-
-const sanityConfig = (() => {
-  if (!process.env.SANITY_STUDIO_API_TOKEN) {
-    throw Error('The API Token is not set. Check your environment variables.');
-  }
-
-  return {
-    dataset: process.env.SANITY_STUDIO_DATASET || 'development',
-    apiVersion: process.env.SANITY_STUDIO_API_VERSION || '2021-10-21',
-    projectId: 'ekrchhx4',
-    token: process.env.SANITY_STUDIO_API_TOKEN,
-    useCdn: process.env.NODE_ENV === 'production',
-  };
-})();
 
 export default createCliConfig({
   api: {
-    ...sanityConfig,
+    projectId: 'ekrchhx4',
+    dataset: process.env.SANITY_STUDIO_DATASET || 'development',
   },
 });
